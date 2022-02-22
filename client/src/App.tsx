f431881dc2bb324173b3761b6ac943672f6dc9bc
F4isalSH <f4isalcode@gmail.com>
2022-02-22 11:23:33 +0300

    Adding react router dom to application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import { ChakraProvider, Box } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export const App = () => (
+export const App: React.FC = () => (
   <ChakraProvider>
-    <Box></Box>
+    <Box>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </Box>
   </ChakraProvider>
 );
