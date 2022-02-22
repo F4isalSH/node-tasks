372ab9e69690146badb1f0e2fa2f9df98d34b3f3
F4isalSH <f4isalcode@gmail.com>
2022-02-22 12:24:33 +0300

    Added form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider, Box } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 export const App: React.FC = () => (
   <ChakraProvider>
     <Box>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </Box>
   </ChakraProvider>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,35 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Heading,
+  HStack,
+  Button,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
 
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = ({}) => {
   return (
-    <Container>
-      <Box>
-        <Heading textAlign="center">Task Manager</Heading>
-      </Box>
-    </Container>
+    <Box h="100vh" bg="#F3F4F6">
+      <Container pt={20}>
+        <VStack bg="white" borderRadius="md" boxShadow="md" p={5} spacing={5}>
+          <Heading textAlign="center">Task Manager</Heading>
+          <HStack>
+            <form>
+              <Input placeholder="e.g wash dishes" />
+              <Button
+                _hover={{ bg: "purple.400" }}
+                color="white"
+                bg="purple.500"
+              >
+                Submit
+              </Button>
+            </form>
+          </HStack>
+        </VStack>
+      </Container>
+    </Box>
   );
 };
