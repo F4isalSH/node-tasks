ef220a585913ff32ed8e7cdb55d7f3d1b151ff63
F4isalSH <f4isalcode@gmail.com>
2022-02-26 19:28:20 +0300

    Validating information

--- server/src/models/task.model.ts
+++ server/src/models/task.model.ts
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    maxlength: [20, "name must not exceed 20 characters"],
+  },
+  completed: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("Task", taskSchema);
