dde59608e3e15b6492b0ee50bb7f23b1d5a7b94e
F4isalSH <f4isalcode@gmail.com>
2022-02-28 22:30:34 +0300

    Created errorHandler middleware

---
+++ server/src/middleware/errorHandler.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+
+const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return res.status(500).json({ msg: err });
+};
